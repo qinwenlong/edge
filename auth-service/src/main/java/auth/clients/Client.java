2d2a5963d95526721cc516a55dd0b3fa61babdf9
qinwenlong <452111141@qq.com>
2019-03-07 16:45:37 +0800

    commit

--- auth-service/src/main/java/auth/clients/Client.java
+++ auth-service/src/main/java/auth/clients/Client.java
@@ -40,4 +40,129 @@ public class Client {
   this.clientId = clientId;
   this.secret = clientSecret;
  }
+ 
+
+public Client() {
+	super();
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getClientId() {
+	return clientId;
+}
+
+public void setClientId(String clientId) {
+	this.clientId = clientId;
+}
+
+public String getSecret() {
+	return secret;
+}
+
+public void setSecret(String secret) {
+	this.secret = secret;
+}
+
+public String getScopes() {
+	return scopes;
+}
+
+public void setScopes(String scopes) {
+	this.scopes = scopes;
+}
+
+public String getAuthorizedGrantTypes() {
+	return authorizedGrantTypes;
+}
+
+public void setAuthorizedGrantTypes(String authorizedGrantTypes) {
+	this.authorizedGrantTypes = authorizedGrantTypes;
+}
+
+public String getAuthorities() {
+	return authorities;
+}
+
+public void setAuthorities(String authorities) {
+	this.authorities = authorities;
+}
+
+public String getAutoApproveScopes() {
+	return autoApproveScopes;
+}
+
+public void setAutoApproveScopes(String autoApproveScopes) {
+	this.autoApproveScopes = autoApproveScopes;
+}
+
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((authorities == null) ? 0 : authorities.hashCode());
+	result = prime * result + ((authorizedGrantTypes == null) ? 0 : authorizedGrantTypes.hashCode());
+	result = prime * result + ((autoApproveScopes == null) ? 0 : autoApproveScopes.hashCode());
+	result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((scopes == null) ? 0 : scopes.hashCode());
+	result = prime * result + ((secret == null) ? 0 : secret.hashCode());
+	return result;
+}
+
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Client other = (Client) obj;
+	if (authorities == null) {
+		if (other.authorities != null)
+			return false;
+	} else if (!authorities.equals(other.authorities))
+		return false;
+	if (authorizedGrantTypes == null) {
+		if (other.authorizedGrantTypes != null)
+			return false;
+	} else if (!authorizedGrantTypes.equals(other.authorizedGrantTypes))
+		return false;
+	if (autoApproveScopes == null) {
+		if (other.autoApproveScopes != null)
+			return false;
+	} else if (!autoApproveScopes.equals(other.autoApproveScopes))
+		return false;
+	if (clientId == null) {
+		if (other.clientId != null)
+			return false;
+	} else if (!clientId.equals(other.clientId))
+		return false;
+	if (id == null) {
+		if (other.id != null)
+			return false;
+	} else if (!id.equals(other.id))
+		return false;
+	if (scopes == null) {
+		if (other.scopes != null)
+			return false;
+	} else if (!scopes.equals(other.scopes))
+		return false;
+	if (secret == null) {
+		if (other.secret != null)
+			return false;
+	} else if (!secret.equals(other.secret))
+		return false;
+	return true;
+}
+ 
 }
